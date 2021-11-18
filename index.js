76e53621f30f80e433842779f3b291f5306451fd
Kshahh <kavyashah2509@gmail.com>
2021-11-18 23:37:15 +0530

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+//API
+const Book = require("./API/book");
+const Author = require("./API/author");
+const Publication = require("./API/publication");
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("connection established!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
 // initialization
 const OurAPP = express();
 
+OurAPP.use(express.json());
+
+// Microservices
+OurAPP.use("/book", Book);
+OurAPP.use("/author", Author);
+OurAPP.use("/publication", Publication);
+
 OurAPP.get("/", (request, response) => {
-    response.json({message: "Request Served!!!!"});
+    response.json({ message: "Server is working!!!!!!" });
 });
 
 OurAPP.listen(4000, () => console.log("Server is running"));
